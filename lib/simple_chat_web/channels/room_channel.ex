83ecde0d44306f64036b3165b517e037347a2eb9
Jean-Serge <js.monbailly@gmail.com>
2021-10-21 19:56:30 +0200

    Simple chat

---
+++ lib/simple_chat_web/channels/room_channel.ex
@@ -0,0 +1,23 @@
+defmodule SimpleChatWeb.RoomChannel do
+  use SimpleChatWeb, :channel
+
+  @impl true
+  def join("room:lobby", payload, socket) do
+    if authorized?(payload) do
+      {:ok, socket}
+    else
+      {:error, %{reason: "unauthorized"}}
+    end
+  end
+
+  @impl Phoenix.Channel
+  def handle_in("new_msg", %{"body" => body}, socket) do
+    broadcast!(socket, "new_msg", %{body: body})
+    {:noreply, socket}
+  end
+
+  # Add authorization logic here as required.
+  defp authorized?(_payload) do
+    true
+  end
+end
